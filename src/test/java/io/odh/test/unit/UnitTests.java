c7f3ee18e916cb9c6d9726fac8f21952f0e0a62d
David Kornel <kornys@outlook.com>
2023-11-09 18:33:36 +0100

    Import opendatahub crds to test suite na drun unit tests (#7)

--- src/test/java/io/odh/test/unit/UnitTests.java
+++ src/test/java/io/odh/test/unit/UnitTests.java
@@ -1,13 +1,19 @@
 package io.odh.test.unit;
 
-import io.fabric8.kubernetes.api.model.PodBuilder;
+import io.fabric8.kubernetes.api.model.KubernetesResourceList;
 import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.dsl.MixedOperation;
+import io.fabric8.kubernetes.client.dsl.Resource;
 import io.fabric8.kubernetes.client.server.mock.EnableKubernetesMockClient;
 import io.fabric8.kubernetes.client.server.mock.KubernetesMockServer;
+import io.opendatahub.datasciencecluster.v1.DataScienceCluster;
+import io.opendatahub.datasciencecluster.v1.DataScienceClusterBuilder;
+import io.opendatahub.datasciencecluster.v1.datascienceclusterspec.ComponentsBuilder;
+import io.opendatahub.datasciencecluster.v1.datascienceclusterspec.components.Codeflare;
+import io.opendatahub.datasciencecluster.v1.datascienceclusterspec.components.CodeflareBuilder;
 import org.junit.jupiter.api.Test;
 
-import java.util.Collections;
-import java.util.Map;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @EnableKubernetesMockClient(crud = true)
 public class UnitTests {
@@ -15,9 +21,28 @@ public class UnitTests {
     private KubernetesMockServer server;
 
     @Test
-    void test() {
-        Map<String, String> matchLabel = Collections.singletonMap("foo", "bar");
-        kubernetesClient.pods().resource(new PodBuilder().withNewMetadata().withName("test").withNamespace("test").endMetadata().build()).create();
+    void testCreateDataScienceCluster() {
+        MixedOperation<DataScienceCluster, KubernetesResourceList<DataScienceCluster>, Resource<DataScienceCluster>> dsClient =
+                kubernetesClient.resources(DataScienceCluster.class);
 
+        DataScienceCluster cluster = new DataScienceClusterBuilder()
+                .withNewMetadata()
+                .withName("Test")
+                .endMetadata()
+                .withNewSpec()
+                .withComponents(
+                        new ComponentsBuilder()
+                                .withCodeflare(
+                                        new CodeflareBuilder()
+                                                .withManagementState(Codeflare.ManagementState.MANAGED)
+                                                .build())
+                                .build())
+                .endSpec()
+                .build();
+
+        dsClient.resource(cluster).create();
+
+        DataScienceCluster returned = dsClient.withName(cluster.getMetadata().getName()).get();
+        assertEquals(Codeflare.ManagementState.MANAGED, returned.getSpec().getComponents().getCodeflare().getManagementState());
     }
 }
