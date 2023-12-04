837e27684cd2de26a7e6aabf8248dd27983898f7
David Kornel <kornys@outlook.com>
2023-12-04 17:25:56 +0100

    Download ODH deployment files during mvn build (#22)
    Signed-off-by: David Kornel <kornys@outlook.com>

---
+++ src/main/java/io/odh/test/install/OlmInstall.java
@@ -0,0 +1,8 @@
+/*
+ * Copyright Skodjob authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+package io.odh.test.install;
+
+public class OlmInstall {
+}
