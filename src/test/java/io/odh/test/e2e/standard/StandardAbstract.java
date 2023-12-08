af54832beb0cad11acc1120c8343a03a710d2bde
David Kornel <kornys@outlook.com>
2023-12-08 10:05:52 +0100

    Use better check for upgrade rediness in bundle upgrade (#36)

--- src/test/java/io/odh/test/e2e/standard/StandardAbstract.java
+++ src/test/java/io/odh/test/e2e/standard/StandardAbstract.java
@@ -14,6 +14,7 @@ import io.odh.test.install.InstallTypes;
 import io.odh.test.install.OlmInstall;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,6 +23,7 @@ import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
+@Tag("standard")
 @ExtendWith(OdhResourceCleaner.class)
 @ExtendWith(ResourceManagerDeleteHandler.class)
 public class StandardAbstract extends Abstract {
