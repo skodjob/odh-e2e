4d59260b85e633b1ed75962d93915e4e9e2779b2
Jakub Stejskal <xstejs24@gmail.com>
2024-01-11 15:25:26 +0100

    Fix typo
    Signed-off-by: Jakub Stejskal <xstejs24@gmail.com>

--- src/main/java/io/odh/test/framework/logs/LogCollector.java
+++ src/main/java/io/odh/test/framework/logs/LogCollector.java
@@ -57,7 +57,7 @@ public class LogCollector {
         Files.writeString(logpath.resolve("pvs.log"), cmdClient.exec(false, false, "describe", "pv").out());
         Files.writeString(logpath.resolve("dsc.yml"), cmdClient.exec(false, false, "get", "dsc", "-o", "yaml").out());
         Files.writeString(logpath.resolve("dsci.yml"), cmdClient.exec(false, false, "get", "dsci", "-o", "yaml").out());
-        Files.writeString(logpath.resolve("subsccriptions.yml"), cmdClient.exec(false, false, "get", "subscriptions.operators.coreos.com", "--all-namespaces", "-o", "yaml").out());
+        Files.writeString(logpath.resolve("subscriptions.yml"), cmdClient.exec(false, false, "get", "subscriptions.operators.coreos.com", "--all-namespaces", "-o", "yaml").out());
         kube.listPodsByPrefixInName(OdhConstants.BUNDLE_OPERATOR_NAMESPACE, "opendatahub-operator-controller-manager").forEach(pod -> {
             writeLogsFromPods(logpath, pod);
         });
