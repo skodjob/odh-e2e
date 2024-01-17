b82fa4aaf191a6c1e7c3282e51c90d883dd95dd9
Jakub Stejskal <xstejs24@gmail.com>
2024-01-17 08:21:27 +0100

    Create log matcher for operator (#64)
    Signed-off-by: Jakub Stejskal <xstejs24@gmail.com>

--- src/main/java/io/odh/test/utils/UpgradeUtils.java
+++ src/main/java/io/odh/test/utils/UpgradeUtils.java
@@ -6,19 +6,16 @@ package io.odh.test.utils;
 
 import io.odh.test.framework.manager.ResourceManager;
 
+import static io.odh.test.framework.matchers.Matchers.logHasNoUnexpectedErrors;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.core.IsNot.not;
 
 public class UpgradeUtils {
 
     public static void deploymentLogIsErrorEmpty(String namespace, String deploymentName) {
-        // Check that operator doesn't contains errors in logs
+        // Check that operator doesn't contain errors in logs
         String operatorLog = ResourceManager.getClient().getClient().apps().deployments()
                 .inNamespace(namespace).withName(deploymentName).getLog();
 
-        assertThat(operatorLog, not(containsString("error")));
-        assertThat(operatorLog, not(containsString("Error")));
-        assertThat(operatorLog, not(containsString("ERROR")));
+        assertThat(operatorLog, logHasNoUnexpectedErrors());
     }
 }
