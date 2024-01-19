7639dae8581194357fe60d07e174630cf1526958
David Kornel <kornys@outlook.com>
2024-01-19 12:44:09 +0100

    Validate that namespace is labelled correctly (#71)
    Signed-off-by: David Kornel <kornys@outlook.com>

--- src/main/java/io/odh/test/TestConstants.java
+++ src/main/java/io/odh/test/TestConstants.java
@@ -18,6 +18,7 @@ public class TestConstants {
     public static final long GLOBAL_POLL_INTERVAL_LONG = Duration.ofSeconds(15).toMillis();
     public static final long GLOBAL_POLL_INTERVAL_MEDIUM = Duration.ofSeconds(10).toMillis();
     public static final long GLOBAL_POLL_INTERVAL_SHORT = Duration.ofSeconds(5).toMillis();
+    public static final long GLOBAL_POLL_INTERVAL_1_SEC = Duration.ofSeconds(1).toMillis();
     public static final long GLOBAL_TIMEOUT = Duration.ofMinutes(10).toMillis();
     public static final long GLOBAL_STABILITY_TIME = Duration.ofMinutes(1).toSeconds();
     public static final String LOG_COLLECT_LABEL = "io.odh-e2e.collect-logs";
