9add95ab4eaa1a9020de83d5bba61d438bfe49e7
Jan Stourac <jstourac@redhat.com>
2024-02-01 12:46:44 +0100

    Added a check for Uninstall scenario (#80)

--- src/test/java/io/odh/test/e2e/standard/DataScienceClusterST.java
+++ src/test/java/io/odh/test/e2e/standard/DataScienceClusterST.java
@@ -4,35 +4,20 @@
  */
 package io.odh.test.e2e.standard;
 
-import io.odh.test.OdhConstants;
 import io.odh.test.TestSuite;
 import io.odh.test.framework.manager.ResourceManager;
-import io.odh.test.framework.manager.requirements.ServiceMeshOperator;
 import io.odh.test.framework.manager.resources.DataScienceClusterResource;
+import io.odh.test.utils.DscUtils;
 import io.opendatahub.datasciencecluster.v1.DataScienceCluster;
-import io.opendatahub.datasciencecluster.v1.DataScienceClusterBuilder;
-import io.opendatahub.datasciencecluster.v1.datascienceclusterspec.ComponentsBuilder;
 import io.opendatahub.datasciencecluster.v1.datascienceclusterspec.components.Codeflare;
-import io.opendatahub.datasciencecluster.v1.datascienceclusterspec.components.CodeflareBuilder;
 import io.opendatahub.datasciencecluster.v1.datascienceclusterspec.components.Dashboard;
-import io.opendatahub.datasciencecluster.v1.datascienceclusterspec.components.DashboardBuilder;
 import io.opendatahub.datasciencecluster.v1.datascienceclusterspec.components.Datasciencepipelines;
-import io.opendatahub.datasciencecluster.v1.datascienceclusterspec.components.DatasciencepipelinesBuilder;
 import io.opendatahub.datasciencecluster.v1.datascienceclusterspec.components.Kserve;
-import io.opendatahub.datasciencecluster.v1.datascienceclusterspec.components.KserveBuilder;
 import io.opendatahub.datasciencecluster.v1.datascienceclusterspec.components.Modelmeshserving;
-import io.opendatahub.datasciencecluster.v1.datascienceclusterspec.components.ModelmeshservingBuilder;
 import io.opendatahub.datasciencecluster.v1.datascienceclusterspec.components.Ray;
-import io.opendatahub.datasciencecluster.v1.datascienceclusterspec.components.RayBuilder;
 import io.opendatahub.datasciencecluster.v1.datascienceclusterspec.components.Trustyai;
-import io.opendatahub.datasciencecluster.v1.datascienceclusterspec.components.TrustyaiBuilder;
 import io.opendatahub.datasciencecluster.v1.datascienceclusterspec.components.Workbenches;
-import io.opendatahub.datasciencecluster.v1.datascienceclusterspec.components.WorkbenchesBuilder;
 import io.opendatahub.dscinitialization.v1.DSCInitialization;
-import io.opendatahub.dscinitialization.v1.DSCInitializationBuilder;
-import io.opendatahub.dscinitialization.v1.dscinitializationspec.Monitoring;
-import io.opendatahub.dscinitialization.v1.dscinitializationspec.ServiceMesh;
-import io.opendatahub.dscinitialization.v1.dscinitializationspec.servicemesh.ControlPlane;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -45,61 +30,11 @@ public class DataScienceClusterST extends StandardAbstract {
 
     @Test
     void createDataScienceCluster() {
-        DSCInitialization dsci = new DSCInitializationBuilder()
-                .withNewMetadata()
-                .withName(OdhConstants.DEFAULT_DSCI_NAME)
-                .endMetadata()
-                .withNewSpec()
-                .withApplicationsNamespace(OdhConstants.CONTROLLERS_NAMESPACE)
-                .withNewMonitoring()
-                .withManagementState(Monitoring.ManagementState.MANAGED)
-                .withNamespace(OdhConstants.MONITORING_NAMESPACE)
-                .endMonitoring()
-                .withNewServiceMesh()
-                .withManagementState(ServiceMesh.ManagementState.MANAGED)
-                .withNewControlPlane()
-                .withName(ServiceMeshOperator.SERVICE_MESH_NAME)
-                .withNamespace(ServiceMeshOperator.SERVICE_MESH_NAMESPACE)
-                .withMetricsCollection(ControlPlane.MetricsCollection.ISTIO)
-                .endControlPlane()
-                .endServiceMesh()
-                .endSpec()
-                .build();
 
-        DataScienceCluster c = new DataScienceClusterBuilder()
-                .withNewMetadata()
-                .withName(DS_PROJECT_NAME)
-                .endMetadata()
-                .withNewSpec()
-                .withComponents(
-                        new ComponentsBuilder()
-                                .withWorkbenches(
-                                        new WorkbenchesBuilder().withManagementState(Workbenches.ManagementState.MANAGED).build()
-                                )
-                                .withDashboard(
-                                        new DashboardBuilder().withManagementState(Dashboard.ManagementState.MANAGED).build()
-                                )
-                                .withKserve(
-                                        new KserveBuilder().withManagementState(Kserve.ManagementState.MANAGED).build()
-                                )
-                                .withCodeflare(
-                                        new CodeflareBuilder().withManagementState(Codeflare.ManagementState.MANAGED).build()
-                                )
-                                .withDatasciencepipelines(
-                                        new DatasciencepipelinesBuilder().withManagementState(Datasciencepipelines.ManagementState.MANAGED).build()
-                                )
-                                .withModelmeshserving(
-                                        new ModelmeshservingBuilder().withManagementState(Modelmeshserving.ManagementState.MANAGED).build()
-                                )
-                                .withRay(
-                                        new RayBuilder().withManagementState(Ray.ManagementState.MANAGED).build()
-                                )
-                                .withTrustyai(
-                                        new TrustyaiBuilder().withManagementState(Trustyai.ManagementState.MANAGED).build()
-                                )
-                                .build())
-                .endSpec()
-                .build();
+        // Create DSCI
+        DSCInitialization dsci = DscUtils.getBasicDSCI();
+        // Create DSC
+        DataScienceCluster c = DscUtils.getBasicDSC(DS_PROJECT_NAME);
 
         ResourceManager.getInstance().createResourceWithWait(dsci);
         ResourceManager.getInstance().createResourceWithWait(c);
